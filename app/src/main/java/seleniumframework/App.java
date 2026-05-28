fc6cdea78fe2d7d665eb1ad0d09f24c18a66f5eb
Soujanya <soujanyachelluboina@gmail.com>
2026-05-28 21:34:05 +0530

    Initial Selenium framework setup

---
+++ app/src/main/java/seleniumframework/App.java
@@ -0,0 +1,18 @@
+package seleniumframework;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class App {
+
+    public static void main(String[] args) {
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://www.google.com");
+
+        System.out.println("Page Title: " + driver.getTitle());
+
+       
+    }
+}
